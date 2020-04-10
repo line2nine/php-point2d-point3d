bac3ed9b25924f4f08d8e72b13a28587cfc18ab4
line2nine <hailinh203@gmail.com>
2020-04-10 16:43:31 +0700

    Add files

--- index.php
+++ index.php
@@ -4,8 +4,8 @@ include_once "class/Point3D.php";
 
 $point2d = new Point2D();
 $point2d->setXY(10, 20);
-echo "Toa Do 2D la: " . $point2d . "<br/>";
+echo " 2D coordinates is: " . $point2d . "<br/>";
 
 $point3d = new Point3D();
 $point3d->setXYZ(100, 200, 300);
-echo "Toa Do 3D la: " . $point3d;
+echo "3D coordinates is: " . $point3d;
